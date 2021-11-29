331d75dbb42963cf2958bd2b1fce77cd085a4ec2
Fernando Hara <fh@spacetimeanalytics.com>
2021-11-29 08:32:20 -0300

    first steps

---
+++ fonte.js
@@ -0,0 +1,19 @@
+{
+    for (var i=1; i<11; i++){
+        if (i % 2 == 1){
+            console.log(i)
+        }
+    }
+}
+{
+    for (var i=1; i<11; i++){
+        if (i % 2 == 0){
+            console.log(i)
+        }
+    }
+}
+
+
+let idade = 31
+console.log(typeof -31)
+console.log(typeof idade)
